7488cc34e0b2a6e3d97bcd80634dd46ac98aa494
Aníbal M. Astobiza <32415432+anibalmastobiza@users.noreply.github.com>
2024-11-01 21:15:59 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,17 @@
+function openTab(evt, tabName) {
+  var i, tabcontent, tablink;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablink = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablink.length; i++) {
+    tablink[i].className = tablink[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Display the first tab by default
+document.getElementsByClassName("tablink")[0].click();
 
